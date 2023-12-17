e20e47544de1ea0f146cb01373e5e7b2c311f200
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-12-16 22:58:37 -0300

    add services: categoria, marca, permissao, pessoa e produto

---
+++ backend/src/main/java/com/estudos/dev/backend/service/PessoaService.java
@@ -0,0 +1,36 @@
+package com.estudos.dev.backend.service;
+
+import com.estudos.dev.backend.entity.Pessoa;
+import com.estudos.dev.backend.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public List<Pessoa> buscarTodos() {
+        return pessoaRepository.findAll();
+    }
+
+    public Pessoa inserir(Pessoa objeto) {
+        objeto.setDataCriacao(new Date());
+        Pessoa objetoNovo = pessoaRepository.saveAndFlush(objeto);
+        return objetoNovo;
+    }
+
+    public Pessoa alterar(Pessoa objeto) {
+        objeto.setDataAtualizacao(new Date());
+        return pessoaRepository.saveAndFlush(objeto);
+    }
+
+    public void excluir(Long id) {
+        Pessoa objeto = pessoaRepository.findById(id).get();
+        pessoaRepository.delete(objeto);
+    }
+}
