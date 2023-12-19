9bc068aa97474af09aa3fe88d819a4c8492ff16f
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-12-19 00:02:03 -0300

    add template para email com freemarker

--- backend/src/main/java/com/estudos/dev/backend/services/EmailService.java
+++ backend/src/main/java/com/estudos/dev/backend/services/EmailService.java
@@ -1,17 +1,28 @@
 package com.estudos.dev.backend.services;
 
+import java.util.Map;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import freemarker.template.Configuration;
 
 @Service
 public class EmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+    @Autowired
+    private Configuration fmConfiguration;
 
     @Value("${spring.mail.username}")
     private String remetente;
@@ -30,4 +41,35 @@ public class EmailService {
         }
 
     }
+
+    public void enviarEmailTemplate(String destinatario, String titulo, Map<String, Object> propriedades) {
+        MimeMessage mimeMessage =javaMailSender.createMimeMessage();
+        try {
+
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+            mimeMessageHelper.setSubject(titulo);
+            mimeMessageHelper.setFrom(remetente);
+            mimeMessageHelper.setTo(destinatario);
+
+            mimeMessageHelper.setText(getConteudoTemplate(propriedades), true);
+
+            javaMailSender.send(mimeMessageHelper.getMimeMessage());
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getConteudoTemplate(Map <String, Object> model)     {
+        StringBuffer content = new StringBuffer();
+
+        try {
+            content.append(FreeMarkerTemplateUtils.processTemplateIntoString(fmConfiguration.getTemplate("email-recuperacao-codigo.flth"), model));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+
+
 }
--- backend/src/main/java/com/estudos/dev/backend/services/PessoaClienteService.java
+++ backend/src/main/java/com/estudos/dev/backend/services/PessoaClienteService.java
@@ -1,6 +1,8 @@
 package com.estudos.dev.backend.services;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +28,11 @@ public class PessoaClienteService {
         pessoa.setDataCriacao(new Date());
         Pessoa objetoNovo = pessoaRepository.saveAndFlush(pessoa);
         permissaoPessoaService.vincularPessoaPermissaoCliente(objetoNovo);
-        emailService.enviarEmailTexto(objetoNovo.getEmail(), "Cadastro na Loja Virtual do Jojo", "O registro na loja foi realizado com sucesso. Em breve você receberá a senha de acesso por e-mail!!");
+        //emailService.enviarEmailTexto(objetoNovo.getEmail(), "Cadastro na Loja Virtual do Jojo", "O registro na loja foi realizado com sucesso. Em breve você receberá a senha de acesso por e-mail!!");
+        Map<String, Object> proprMap = new HashMap<>();
+        proprMap.put("nome", objetoNovo.getNome());
+        proprMap.put("mensagem", "O registro na loja foi realizado com sucesso. Em breve você receberá a senha de acesso por e-mail!");
+        emailService.enviarEmailTemplate(objetoNovo.getEmail(), "Cadastro na Loja Virtual do Jojo", proprMap);
         return objetoNovo;
     }
 
